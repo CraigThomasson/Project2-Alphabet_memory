48e6ee8654490be13fa828354b012ef2171e45fd
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-08-22 15:47:24 +0000

    add style to game cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
-function smallGame() {
-    let board = document.getElementById("board");
-    let newDiv = `<div id="small-grid"></div>`;
-    board.innerHTML = newDiv;
-    let cardContainer = "";
-    for (let i = 0; i < 18; i++) {
-    cardContainer += `<div class="box${i}"><h1>hi${i}</h1></div>`;
-    }
-    let container = document.getElementById("small-grid");
-    container.innerHTML = cardContainer;
+let board = document.getElementById("board");
+let newDiv = `<div id="small-grid"></div>`;
+board.innerHTML = newDiv;
+let cardContainer = "";
+for (let i = 0; i < 16; i++) {
+   cardContainer += `<div class="card box${i}"><h1>hi${i}</h1></div>`;
 }
-
+let container = document.getElementById("small-grid");
+container.innerHTML = cardContainer;
